2a06a45c4a0cba34b3ee67c9cabeec170aff5411
TopsterTX <bbs.100@yandex.ru>
2021-06-16 23:39:14 +0300

    Поработал над СТАТИЧНЫМ слайдером

--- src/components/Main/header/location/Location.jsx
+++ src/components/Main/header/location/Location.jsx
@@ -4,7 +4,7 @@ import './Location.scss'
 export const Location = () => {
     return(
         <section className='location'>
-            <img src="'../../../../icons/location.svg'" alt="123" className='location__icon'/>
+            <img src="'../../../../icons/location.svg'" alt="" className='location__icon'/>
             <span className='location__place'>Ульяновск</span>
         </section>
     )
