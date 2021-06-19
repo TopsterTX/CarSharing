00cb6bb3cb613b0aa9b64dc08cc3835a75baeca9
TopsterTX <bbs.100@yandex.ru>
2021-06-19 14:12:49 +0300

    Мелкие правки

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import { HomePage } from "./components/HomePage/HomePage";
-import { OrderPage } from "./components/OrderPage/OrderPage";
+
 import "./App.scss";
 
 function App() {
   return (
+
+    
     <BrowserRouter>
-      <Switch>
-        <Route path="/home" component={HomePage} exact={true} />
-        <Route path="/order" component={OrderPage}></Route>
-        <Redirect to="/home" />
-      </Switch>
+      <HomePage />
     </BrowserRouter>
   );
 }
