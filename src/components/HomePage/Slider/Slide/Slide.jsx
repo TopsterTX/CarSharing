95cf50b8fd72fa3e97135f13feff4f8c1b8ed06c
TopsterTX <bbs.100@yandex.ru>
2021-06-28 13:52:15 +0300

    F-01 Переделал Слайдер

--- src/components/HomePage/Slider/Slide/Slide.jsx
+++ src/components/HomePage/Slider/Slide/Slide.jsx
@@ -1,19 +1,47 @@
-import React from 'react'
-import './Slide.scss'
+import React from "react";
+import "./Slide.scss";
 
-export const Slide = ({img, title, text}) => {
-    return (
-        <section className='slide'>
-            <img src={img} alt="" className='slide__image'/>
-            <div className="slide__wrapper">
-                <div className="slide__arrow slide__arrow--left" />
-                <div className="slide__content">
-                    <h2>{title}</h2>
-                    <p>{text}</p>
-                    <button className='slide__button'>Подробнее</button>
-                </div>
-                <div className="slide__arrow slider__arrow--right" />
-            </div>
-        </section>
-    )
-}
+export const Slide = ({ img, title, text, active, setValue, value, dataLenght }) => {
+  const arrowLeftHandler = () => {
+    if (value > 0) {
+      return setValue(value - 1);
+    } else if (value <= 0) {
+      return setValue(dataLenght);
+    }
+    console.log("left", value);
+  };
+
+  const arrowRightHandler = () => {
+    if (value < dataLenght) {
+      return setValue(value + 1);
+    } else if (value >= dataLenght) {
+      return setValue(0);
+    }
+    console.log("right", value);
+  };
+
+  return (
+    <section className={`slide ${active ? "active" : ""}`}>
+      <img src={img} alt="" className="slide__image" />
+      <div className="slide__wrapper">
+        <div
+          className="slide__arrow slide__arrow--left"
+          onClick={() => {
+            arrowLeftHandler();
+          }}
+        />
+        <div className="slide__content">
+          <h2>{title}</h2>
+          <p>{text}</p>
+          <button className="slide__button">Подробнее</button>
+        </div>
+        <div
+          className="slide__arrow slider__arrow--right"
+          onClick={() => {
+            arrowRightHandler();
+          }}
+        />
+      </div>
+    </section>
+  );
+};
--- src/components/HomePage/Slider/Slider.jsx
+++ src/components/HomePage/Slider/Slider.jsx
@@ -1,51 +1,60 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { ContentSlider } from "./ContentSlider/ContentSlider";
 import { Arrow } from "./Arrow/Arrow";
 import { Carousel } from "react-responsive-carousel";
-import {Slide} from './Slide/Slide'
+import { Slide } from "./Slide/Slide";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import slide1 from "../../../images/slider-1.jpg";
+import slide2 from "../../../images/slider-2.jpg";
+import slide3 from "../../../images/slider-3.jpg";
+import slide4 from "../../../images/slider-4.jpg";
 import "./Slider.scss";
 
 export const Slider = () => {
-  let sliderData = [
+  const sliderData = [
     {
-      img: 'https://www.nastol.com.ua/download.php?img=201705/1920x1200/nastol.com.ua-226206.jpg',
+      img: slide1,
       text: "Оставляйте машину на платных городских парковках и разрешенных местах, не нарушая ПДД, а также в аэропортах.",
       title: "Бесплатная парковка",
       id: 0,
     },
     {
-      img: 'https://forum.awd.ru/gallery/images/upload/224/007/2240077493520d925efcc3146f71faf7.jpg',
+      img: slide2,
       text: "Полная страховка страховка автомобиля",
       title: "Страховка",
       id: 1,
     },
     {
-      img: 'https://s3.nat-geo.ru/images/2020/10/4/a056499dce954c7daaa84e28f6ed8a6c.max-2500x1500.jpg',
+      img: slide3,
       text: "Полный бак на любой заправке города за наш счёт",
       title: "Бензин",
       id: 2,
     },
-    // {
-    //   text: "Автомобиль проходит еженедельное TO",
-    //   title: "Обслуживание",
-    //   id: 3,
-    // },
-    // {
-    //   text: "LOREM",
-    //   title: "LOREM",
-    //   id: 4,
-    // },
+    {
+      img: slide4,
+      text: "Автомобиль проходит еженедельное TO",
+      title: "Обслуживание",
+      id: 3,
+    },
   ];
 
+  const [value, setValue] = useState(0);
   return (
     <section className="slider">
-      <Carousel autoPlay>
-        {sliderData.map((el, index) => {
-          return(
-            <Slide img={el.img} title={el.title} text={el.text} key={index}/>
-          )
-        })}
-      </Carousel>
+      {sliderData.map((el) => {
+        return (
+          <Slide
+            img={el.img}
+            text={el.text}
+            title={el.title}
+            id={el.id}
+            active={value === el.id ? true : false}
+            setValue={setValue}
+            value={value}
+            dataLenght={sliderData.length - 1}
+          />
+        );
+      })}
     </section>
   );
 };
