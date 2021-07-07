99af60203166276b182335647b90620b3cdc2584
TopsterTX <bbs.100@yandex.ru>
2021-07-08 02:35:09 +0300

    V-03 Добавил файл с миксинами

--- src/components/OrderPage/OrderPage.jsx
+++ src/components/OrderPage/OrderPage.jsx
@@ -3,6 +3,7 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import { Footer } from "../HomePage/Main/Footer/Footer";
 import { Header } from "../HomePage/Main/Header/Header";
 import  Steps  from "../OrderPage/Steps/Steps";
+// import {Options} from './Options/Options'
 import Place from "./Place/Place";
 import { Total } from "./Total/Total";
 import "./OrderPage.scss";
@@ -17,6 +18,7 @@ export const OrderPage = () => {
         <Switch>
           <Route path="/order/place" render={() => <Place />} />
           <Route path="/order/models" render={() => <Models />} />
+          {/* <Route path='/order/option' render={() => <Options/> }/> */}
         </Switch>
         <Total />
       </div>
